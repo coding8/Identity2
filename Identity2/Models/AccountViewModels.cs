51b1d261defc4e547ec60a673450c6ba56c6c927
O <lesson6@126.com>
2014-08-28 15:34:46 +0800

    管理账户：密码位数

--- Identity2/Models/AccountViewModels.cs
+++ Identity2/Models/AccountViewModels.cs
@@ -24,7 +24,7 @@ namespace Identity2.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "新密码")]
         public string NewPassword { get; set; }
